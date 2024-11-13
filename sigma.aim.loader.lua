da0db8b0098c0f356aa4cf2bdb887007e199f629
AndreyG_ <andreythedev2@gmail.com>
2024-11-13 16:25:21 +0500

    Update sigma.aim.loader.lua

--- sigma.aim.loader.lua
+++ sigma.aim.loader.lua
@@ -1,8 +1,3 @@
 if script then
-  if PlaceId not "2746687316" then
-    print("Broo, its wrong place 💀💀")
-  else
-
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/AndreyTheDev/sigma/refs/heads/main/sigma.loader/sigma.aim.lua'))() -- btw, you can view my bad code :3
-  end
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/AndreyTheDev/sigma/refs/heads/main/sigma.loader/sigma.aim.lua'))() -- btw, you can view my bad code :3
 end
