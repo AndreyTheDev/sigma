ca49d400df61880b19f8788794a93c0a660853ea
AndreyG_ <andreythedev2@gmail.com>
2024-11-13 16:26:59 +0500

    Update sigma.aim.loader.lua

--- sigma.aim.loader.lua
+++ sigma.aim.loader.lua
@@ -1,3 +1,4 @@
 if script then
   loadstring(game:HttpGet('https://raw.githubusercontent.com/AndreyTheDev/sigma/refs/heads/main/sigma.loader/sigma.aim.lua'))() -- btw, you can view my bad code :3
 end
+
