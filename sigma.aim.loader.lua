6594a0ff33a2fe18af23dee792ffe08a29f53542
AndreyG_ <andreythedev2@gmail.com>
2024-11-25 22:33:00 +0500

    Update sigma.aim.loader.lua

--- sigma.aim.loader.lua
+++ sigma.aim.loader.lua
@@ -1,1 +1,5 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/AndreyTheDev/sigma/refs/heads/main/sigma.loader/sigma.aim.lua'))() 
+if game.GameId == 992334288 then
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/AndreyTheDev/sigma/refs/heads/main/sigma.loader/sigma.aim.lua'))() 
+else
+  print("[SIGMA]: Wrong place")
+end
