43d8d649e60a18e0e06294da305c49d5e6261aa1
andreythedevv <andreythedev2@gmail.com>
2025-09-09 01:04:49 +0500

    Create loader.lua

---
+++ sigma.runhidefight/loader.lua
@@ -0,0 +1,5 @@
+if game.GameId == 135406051460913 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/AndreyTheDev/sigma/main/sigma.runhidefight/main.lua"))()
+else
+  print("[SIGMA]: Wrong place")
+end
