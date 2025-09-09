106e8a86ab206b4942846c1af28835d2c3886658
andreythedevv <andreythedev2@gmail.com>
2025-09-09 18:08:04 +0500

    Update loader.lua

--- sigma.runhidefight/loader.lua
+++ sigma.runhidefight/loader.lua
@@ -1,5 +1,5 @@
-if game.GameId == 135406051460913 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/AndreyTheDev/sigma/main/sigma.runhidefight/main.lua"))()
+if game.GameId == 135406051460913 or game.PlaceId == 135406051460913 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AndreyTheDev/sigma/main/sigma.runhidefight/main.lua"))()
 else
-  print("[SIGMA]: Wrong place")
+    print("[SIGMA]: Wrong place | GameId: " .. game.GameId .. " | PlaceId: " .. game.PlaceId)
 end
